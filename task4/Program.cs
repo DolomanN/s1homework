53147ad5ab6fe133b8768c181dbb6b304224add5
Nikita <nikitya39@gmail.com>
2022-03-02 22:04:46 +0200

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int NumberN = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count < NumberN)
+{
+    Console.WriteLine(count);
+    count += 2;
+}
+
+
